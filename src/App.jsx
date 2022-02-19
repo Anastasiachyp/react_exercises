a348159861abdfaacbed77d26a566250897099bc
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-19 17:48:39 +0100

    add button function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 function App() {
+  const print = () => {
+    console.log('print me')
+  }
   return (
     <div>
       <h2>Hello World!</h2>
+      <button onClick={print}>click</button>
     </div>
   )
 }
